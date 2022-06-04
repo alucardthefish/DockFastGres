a41ec41b243abd55890135a5783f9970da457ea0
alucardthefish <sergiop18@gmail.com>
2022-06-04 16:42:22 -0500

    Complete CRUD operations for User model API

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ from dotenv import load_dotenv
 from fastapi_sqlalchemy import DBSessionMiddleware, db
 from models import User as ModelUser
 from schema import User as SchemaUser
-from sqlalchemy.orm import Session
-from user_crud import get_user_by_id, get_users
+from user_crud import create_user, delete_user, get_user_by_id, get_users, update_user
 
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -19,15 +18,8 @@ app = FastAPI()
 app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"])
 
 @app.post("/user/", response_model=SchemaUser)
-def create_user(user: SchemaUser):
-    db_user = ModelUser(
-        first_name=user.first_name,
-        last_name=user.last_name,
-        age=user.age
-    )
-    db.session.add(db_user)
-    db.session.commit()
-    return db_user
+def make_user(user: SchemaUser):
+    return create_user(db.session, user)
 
 @app.get("/users/", status_code=200)
 def load_users():
@@ -38,6 +30,15 @@ def load_users():
 def load_user(user_id: int):
     user = get_user_by_id(db.session, user_id)
     return user
+
+@app.put("/user/{user_id}", response_model=SchemaUser, status_code=200)
+def modify_user(user_id: int, user_data: SchemaUser):
+    return update_user(db.session, user_id, user_data)
+
+@app.delete("/user/{user_id}", response_model=SchemaUser, status_code=200)
+def remove_user_by_id(user_id: int):
+    was_deleted = delete_user(db.session, user_id)
+    return was_deleted
 
 
 if __name__ == "__main__":
